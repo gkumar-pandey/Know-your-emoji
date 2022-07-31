388a2e0d2819edde4d8b0b7f69ccf41242dbedeb
gkumar-pandey <gautamshekhar078@gmail.com>
2022-07-31 14:50:36 +0530

    add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import emojiDetails from "./Components/Data/Data";
+import { useState } from "react";
 
 function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("");
+  const emoji = Object.keys(emojiDetails);
+  const renderEmojiMeaning = (text) => {
+    if (text == undefined) {
+      setEmojiMeaning("This emoji is not available in our Database");
+    } else if (text == "") {
+      setEmojiMeaning("Your emoji meaning will appear here");
+    } else {
+      setEmojiMeaning(text);
+    }
+  };
+  const clickHandle = (e) => {
+    let text = emojiDetails[e];
+    renderEmojiMeaning(text);
+  };
+  const inputHandle = (e) => {
+    console.log(e.target.value);
+    let text = emojiDetails[e.target.value];
+    renderEmojiMeaning(text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-wrapper">
+        <h2 className="heading">Know Your Emoji</h2>
+        <input
+          type="text"
+          id="input-bar"
+          placeholder="Put your emoji here to know the meaning"
+          onChange={inputHandle}
+        />
+        <div id="output-text">{emojiMeaning}</div>
+        <div id="emoji-we-have">
+          {emoji.map((item) => {
+            return (
+              <span
+                key={item}
+                style={style.emoji}
+                onClick={() => clickHandle(item)}
+              >
+                {item}
+              </span>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+
+const style = {
+  emoji: {
+    fontSize: "2rem",
+    padding: "1rem",
+  },
+};
 
 export default App;
---
+++ src/Components/Data/Data.js
@@ -0,0 +1,17 @@
+const emojiDetails = {
+  "😊": "Smiling",
+  "😳": "Disbelief",
+  "😔": "Sad",
+  "🥡": "Takeout box",
+  "❤️": "Love",
+  "😴": "Sleepy",
+  "😎": "Cool",
+  "🤒": "Sick",
+  "👿": "Angry",
+  "🥰": "Feeling loved",
+  "😑": "Annoyance",
+  "🥳": "Party",
+  "🥺": "Begging",
+  "🧿": "Evil Eye",
+};
+export default emojiDetails;
